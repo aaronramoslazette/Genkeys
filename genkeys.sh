5b112114e4599e7f5a3f53f41e0c8fea8a44222b
aaronramoslazette <aaronramoslazette@cmail.carleton.ca>
2019-01-29 11:52:25 -0500

    Functionalized and removed output redirection

--- genkeys.sh
+++ genkeys.sh
@@ -4,31 +4,71 @@
 
 #!/bin/bash
 
-echo "#########################################################################"
-echo "################################# GENKEYS ################################"
-echo "#########################################################################"
+###############
+## Functions ##
+###############
 
-printf "\n"
-echo -e "\e[3mA Github SSH Key Automation tool by Aaron Ramos-Lazette\e[0m"
-printf "\n\nPlease enter the email associated with your Github account:\n"
+rootCheck()
+{
+  if [ $(id -u) != "0" ]
+  then
+    sudo "$0" "@"
+    exit $?
+  fi
+}
 
-read email
+about()
+{
+  echo "#########################################################################"
+  echo "################################# GENKEYS ################################"
+  echo "#########################################################################"
 
-printf "\n\nSSH Key is about to be generated."
-printf "\nWhen prompted, press ENTER to use the default settings.\n\n"
+  printf "\n"
+  echo   "\e[3mA Github SSH Key Automation tool by Aaron Ramos-Lazette\e[0m"
+}
 
-ssh-keygen -t rsa -b 4096 -C $email
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+getEmail()
+{
+  printf "\n\nPlease enter the email associated with your Github account:\n"
 
-printf "\n\nInstalling the xclip tool to assist in retrieving your new key."
-printf "\nPlease enter your user credentials when prompted.\n\n"
-$(sudo apt-get install xclip) #&> /dev/null/
-$(xclip -sel clip < ~/.ssh/id_rsa.pub) #&> /dev/null/
+  read email
+}
 
-printf "\n\nYour new ssh key has been added to your clipboard."
-printf "\nLogin to your Github account and visit https://github.com/settings/ssh/new"
-printf "and paste this new key."
-printf "\nPress ENTER once this step is complete."
+genKey()
+{
+  printf "\n\nSSH Key is about to be generated."
+  printf "\nWhen prompted, press ENTER to use the default settings.\n\n"
 
-printf "\nYou should now be ready to clone and push to private repos."
+  ssh-keygen -t rsa -b 4096 -C $email
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+}
+
+installXclip()
+{
+  printf "\n\nInstalling the xclip tool to assist in retrieving your new key."
+  printf "\nPlease enter your user credentials if prompted.\n\n"
+  sudo apt-get install xclip
+  xclip -sel clip < ~/.ssh/id_rsa.pub
+}
+
+finalWords()
+{
+  printf "\n\nYour new ssh key has been added to your clipboard."
+  printf "\nLogin to your Github account and visit https://github.com/settings/ssh/new"
+  printf "and paste this new key."
+  printf "\nPress ENTER once this step is complete."
+
+  printf "\nYou should now be ready to clone and push to private repos."
+}
+
+#############
+## PROGRAM ##
+#############
+
+rootCheck "${@}"
+about
+getEmail
+genKey
+installXclip
+finalWords
